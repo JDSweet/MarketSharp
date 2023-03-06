7a538038c7250e9734f7bc9e7a4aa6a3f81afd2a
JDSweetBeat <32932271+JDSweet@users.noreply.github.com>
2023-03-06 13:47:12 -0600

    Create GoodData.cs

---
+++ Src/GoodData.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GoodData
+{
+    public string tag;
+    public float basePrice;
+    public float max, min;
+    public float price;
+    public float buys;
+    public float sells;
+}
